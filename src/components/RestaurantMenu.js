a7d5443d865be69fdc98c540dc9c9c396d04c9de
Naga Pradeep Bhupalam <pbhupalam@salesforce.com>
2024-03-11 23:53:35 +0530

    Collapsing all items

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -14,12 +14,20 @@ const RestaurantMenu = () => {
         return eachItem.card?.card["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory";
     });
 
+    const onCategoryClick = (newIndex) => {
+        if(showIndex === newIndex) {
+            setShowIndex(null);
+        } else {
+            setShowIndex(newIndex);
+        }
+    }
+
     return (
         <div className='max-w-[50%] mx-auto'>
             <h1 className='font-bold text-xl'>{name}</h1>
             <p>{cuisines.join(', ')}</p>
             {itemCategories.map((m, i) => {
-                return <RestaurantCategory data={m?.card?.card} key={i} showItems={i === showIndex} onCategoryClick={setShowIndex} itemIndex={i} />
+                return <RestaurantCategory data={m?.card?.card} key={i} showItems={i === showIndex} onCategoryClick={onCategoryClick} itemIndex={i} />
             })}
         </div>
     );
