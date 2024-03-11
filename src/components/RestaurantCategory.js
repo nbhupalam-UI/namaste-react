c3b9e6d461b49c80199220df6f2cda2fce9cf711
Naga Pradeep Bhupalam <pbhupalam@salesforce.com>
2024-03-11 23:48:34 +0530

    ex-11: refactor using index instead of title for expand and collapse

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,9 +1,9 @@
 import ItemList from "./ItemList";
 
-const RestaurantCategory = ({ data, onCategoryClick, showItems }) => {
+const RestaurantCategory = ({ data, onCategoryClick, showItems, itemIndex }) => {
   return (
     <div className="mt-4 mb-6 shadow-lg bg-gray-100 p-4 rounded">
-      <button className="flex justify-between w-full" onClick={() => onCategoryClick(data.title)}>
+      <button className="flex justify-between w-full" onClick={() => onCategoryClick(itemIndex)}>
         <span className="font-bold">{data.title} ({data.itemCards.length})</span>
         <span className={`${showItems ? 'rotate-180' : ''}`}>⬇️</span>
       </button>
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -5,7 +5,7 @@ import RestaurantCategory from './RestaurantCategory';
 import { useState } from 'react';
 
 const RestaurantMenu = () => {
-    const [title, setTitle] = useState('');
+    const [showIndex, setShowIndex] = useState(0);
     const { resId } = useParams();
     const resData = useRestaurantMenu(resId);
     if(resData === null) return;
@@ -19,7 +19,7 @@ const RestaurantMenu = () => {
             <h1 className='font-bold text-xl'>{name}</h1>
             <p>{cuisines.join(', ')}</p>
             {itemCategories.map((m, i) => {
-                return <RestaurantCategory data={m?.card?.card} key={i} showItems={m?.card?.card?.title === title} onCategoryClick={setTitle} />
+                return <RestaurantCategory data={m?.card?.card} key={i} showItems={i === showIndex} onCategoryClick={setShowIndex} itemIndex={i} />
             })}
         </div>
     );
